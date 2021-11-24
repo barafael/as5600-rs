b6ef7831239f80cb7b279256cfa8f24b9c2fb3c3
Rafael Bachmann <rafael.bachmann.93@gmail.com>
2021-11-24 23:59:34 +0100

    Mark basic types as copy

--- src/configuration/conversion.rs
+++ src/configuration/conversion.rs
@@ -1,7 +1,8 @@
 use crate::configuration::error::Error;
 
 use super::{
-    FastFilterThreshold, Hysteresis, OutputStage, PowerMode, PwmFreq, SlowFilter, WatchdogState,
+    Configuration, FastFilterThreshold, Hysteresis, OutputStage, PowerMode, PwmFreq, SlowFilter,
+    WatchdogState,
 };
 
 impl From<u8> for PowerMode {
@@ -166,5 +167,51 @@ impl From<WatchdogState> for u8 {
             WatchdogState::Off => 0,
             WatchdogState::On => 1,
         }
+    }
+}
+
+impl TryFrom<u16> for Configuration {
+    type Error = Error;
+    fn try_from(bytes: u16) -> Result<Self, Self::Error> {
+        let pm = (bytes & 0b0000_0000_0000_0011) as u8;
+        let hyst = ((bytes & 0b0000_0000_0000_1100) >> 2) as u8;
+        let outs = ((bytes & 0b0000_0000_0011_0000) >> 4) as u8;
+        let pwmf = ((bytes & 0b0000_0000_1100_0000) >> 6) as u8;
+        let sf = ((bytes & 0b0000_0011_0000_0000) >> 8) as u8;
+        let fth = ((bytes & 0b0001_1100_0000_0000) >> 10) as u8;
+        let wd = ((bytes & 0b0010_0000_0000_0000) >> 13) as u8;
+        Ok(Self {
+            power_mode: pm.into(),
+            hysteresis: hyst.into(),
+            output_stage: outs.try_into()?,
+            pwm_frequency: pwmf.into(),
+            slow_filter: sf.into(),
+            fast_filter_threshold: fth.into(),
+            watchdog_state: wd.into(),
+            fields: bytes,
+        })
+    }
+}
+
+impl From<Configuration> for u16 {
+    fn from(config: Configuration) -> Self {
+        let mut fields: u16 = 0;
+        let power_mode_bits = u8::from(config.power_mode) as u16;
+        fields |= power_mode_bits;
+        let hyst_bits = (u8::from(config.hysteresis) as u16) << 2;
+        fields |= hyst_bits;
+        let outs_bits = (u8::from(config.output_stage) as u16) << 4;
+        fields |= outs_bits;
+        let pwmf_bits = (u8::from(config.pwm_frequency) as u16) << 6;
+        fields |= pwmf_bits;
+        let sf_bits = (u8::from(config.slow_filter) as u16) << 8;
+        fields |= sf_bits;
+        let fth_bits = (u8::from(config.fast_filter_threshold) as u16) << 10;
+        fields |= fth_bits;
+        let wd_bits = (u8::from(config.watchdog_state) as u16) << 13;
+        fields |= wd_bits;
+        // Restore 2 top-most bits.
+        fields |= config.fields & 0b1100_0000_0000_0000;
+        fields
     }
 }
--- src/configuration/mod.rs
+++ src/configuration/mod.rs
@@ -1,14 +1,12 @@
 #[cfg(test)]
 use proptest_derive::Arbitrary;
 
-use self::error::Error;
-
 mod conversion;
 pub mod error;
 #[cfg(test)]
 mod test;
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum PowerMode {
     Nom,
@@ -17,7 +15,7 @@ pub enum PowerMode {
     Lpm3,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum Hysteresis {
     Off,
@@ -26,7 +24,7 @@ pub enum Hysteresis {
     Lsb3,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum OutputStage {
     Analog,
@@ -34,7 +32,7 @@ pub enum OutputStage {
     DigitalPwm,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum PwmFreq {
     PwmF1 = 115,
@@ -44,12 +42,12 @@ pub enum PwmFreq {
 }
 
 impl PwmFreq {
-    pub fn to_hz(freq: PwmFreq) -> usize {
-        freq as usize
+    pub fn to_hz(freq: &PwmFreq) -> usize {
+        *freq as usize
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum SlowFilter {
     X16,
@@ -58,7 +56,7 @@ pub enum SlowFilter {
     X2,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum FastFilterThreshold {
     SlowFilterOnly,
@@ -71,14 +69,14 @@ pub enum FastFilterThreshold {
     Lsb10,
 }
 
-#[derive(Debug, PartialEq, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub enum WatchdogState {
     On,
     Off,
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Copy, Clone)]
 #[cfg_attr(test, derive(Arbitrary))]
 pub struct Configuration {
     pub power_mode: PowerMode,
@@ -89,52 +87,6 @@ pub struct Configuration {
     pub fast_filter_threshold: FastFilterThreshold,
     pub watchdog_state: WatchdogState,
     pub fields: u16, // See note in datasheet about "blank fields may contain factory settings" on page 18
-}
-
-impl TryFrom<u16> for Configuration {
-    type Error = Error;
-    fn try_from(bytes: u16) -> Result<Self, Self::Error> {
-        let pm = (bytes & 0b0000_0000_0000_0011) as u8;
-        let hyst = ((bytes & 0b0000_0000_0000_1100) >> 2) as u8;
-        let outs = ((bytes & 0b0000_0000_0011_0000) >> 4) as u8;
-        let pwmf = ((bytes & 0b0000_0000_1100_0000) >> 6) as u8;
-        let sf = ((bytes & 0b0000_0011_0000_0000) >> 8) as u8;
-        let fth = ((bytes & 0b0001_1100_0000_0000) >> 10) as u8;
-        let wd = ((bytes & 0b0010_0000_0000_0000) >> 13) as u8;
-        Ok(Self {
-            power_mode: pm.into(),
-            hysteresis: hyst.into(),
-            output_stage: outs.try_into()?,
-            pwm_frequency: pwmf.into(),
-            slow_filter: sf.into(),
-            fast_filter_threshold: fth.into(),
-            watchdog_state: wd.into(),
-            fields: bytes,
-        })
-    }
-}
-
-impl From<Configuration> for u16 {
-    fn from(config: Configuration) -> Self {
-        let mut fields: u16 = 0;
-        let power_mode_bits = u8::from(config.power_mode) as u16;
-        fields |= power_mode_bits;
-        let hyst_bits = (u8::from(config.hysteresis) as u16) << 2;
-        fields |= hyst_bits;
-        let outs_bits = (u8::from(config.output_stage) as u16) << 4;
-        fields |= outs_bits;
-        let pwmf_bits = (u8::from(config.pwm_frequency) as u16) << 6;
-        fields |= pwmf_bits;
-        let sf_bits = (u8::from(config.slow_filter) as u16) << 8;
-        fields |= sf_bits;
-        let fth_bits = (u8::from(config.fast_filter_threshold) as u16) << 10;
-        fields |= fth_bits;
-        let wd_bits = (u8::from(config.watchdog_state) as u16) << 13;
-        fields |= wd_bits;
-        // Restore 2 top-most bits.
-        fields |= config.fields & 0b1100_0000_0000_0000;
-        fields
-    }
 }
 
 impl PartialEq for Configuration {
